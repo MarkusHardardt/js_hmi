f0ac8ddbfd4458df5eabb20f701e47780570e6e9
Markus Hardardt <markus.hardardt@gmx.ch>
2026-02-04 16:59:45 +0100

    working on opc ua client handling

--- src/OPCUA.js
+++ src/OPCUA.js
@@ -2,8 +2,10 @@
     "use strict";
     const OPCUA = {};
     const isNodeJS = typeof require === 'function';
-    const { OPCUAClient, DataType, AttributeIds, resolveNodeId } = require('node-opcua-client');
+    const fs = require('fs');
+    const { OPCUAClient, DataType, AttributeIds, TimestampsToReturn, ClientSubscription, resolveNodeId } = require('node-opcua-client');
     const Executor = require('@markus.hardardt/js_utils/src/Executor.js');
+    const Regex = require('@markus.hardardt/js_utils/src/Regex.js');
     const Core = require('@markus.hardardt/js_utils/src/Core.js');
 
     function getAsCoreDataType(type) {
@@ -53,11 +55,34 @@
     }
     OPCUA.getAsCoreDataType = getAsCoreDataType;
 
+    const keyValueRegex = /^([_a-z0-9]+);(.+)$/i;
+    function loadKeysAndValuesFromCSVFile(file, onSuccess, onError) {
+        try {
+            const result = {};
+            const lines = fs.readFileSync(file, 'utf8').split(Regex.Linebreaks);
+            for (const line of lines) {
+                const match = keyValueRegex.exec(line);
+                if (match) {
+                    const key = match[1];
+                    if (result[key] !== undefined) {
+                        onError(`Duplicate key found: '${key}'`);
+                        return;
+                    }
+                    result[key] = match[2];
+                }
+            }
+            onSuccess(result);
+        } catch (error) {
+            onError(`Failed reading csv file '${file}': '${error.message}'`);
+        }
+    }
+    OPCUA.loadKeysAndValuesFromCSVFile = loadKeysAndValuesFromCSVFile;
+
     function getAccessString(namespace, nodeId) {
         return `ns=${namespace};s=${nodeId}`;
     }
 
-    async function startOpcuaClient(client, endpointUrl, namespace, nodeIds, nodes) {
+    async function startOpcuaClientAsync(client, endpointUrl, nodes) {
         try {
             console.log(`Connecting OPC UC client to endpointUrl: ${endpointUrl}`);
             await client.connect(endpointUrl);
@@ -65,17 +90,21 @@
             const session = await client.createSession();
             console.log('Successfully connected!');
 
-            for (const id of nodeIds) {
-                try {
-                    const nodeId = resolveNodeId(getAccessString(namespace, id));
-                    const dataValue = await session.read({ nodeId, attributeId: AttributeIds.Value });
-                    if (dataValue.statusCode.name === 'Good') {
-                        nodes[id] = { nodeId, value: dataValue.value.value, type: dataValue.value.dataType };
-                    } else {
-                        console.error(`❌  NodeId ${id} exists, but status: ${dataValue.statusCode.name}`);
+            for (const dataId in nodes) {
+                if (nodes.hasOwnProperty(dataId)) {
+                    const node = nodes[dataId];
+                    try {
+                        const dataValue = await session.read({ nodeId: node.nodeId, attributeId: AttributeIds.Value });
+                        if (dataValue.statusCode.name === 'Good') {
+                            node.value = dataValue.value.value;
+                            node.rawType = dataValue.value.dataType;
+                            node.type = getAsCoreDataType(dataValue.value.dataType);
+                        } else {
+                            console.error(`❌  NodeId ${id} exists, but status: ${dataValue.statusCode.name}`);
+                        }
+                    } catch (err) {
+                        console.error(`❌ NodeId ${id} could not be read:`, err.message);
                     }
-                } catch (err) {
-                    console.error(`❌ NodeId ${id} could not be read:`, err.message);
                 }
             }
             return session;
@@ -84,9 +113,18 @@
         }
     }
 
-    async function stopOpcuaClient(session, client) {
+    async function stopOpcuaClientAsync(session, client, subscription, nodes) {
         try {
             console.log('cleaning up ...');
+            for (const dataId in nodes) {
+                if (nodes.hasOwnProperty(dataId)) {
+                    const node = nodes[dataId];
+                    if (node.monitoredItem) {
+                        await node.monitoredItem.terminate();
+                    }
+                }
+            }
+            await subscription.terminate();
             await session.close();
             await client.disconnect();
             console.log('cleanup done');
@@ -95,28 +133,73 @@
         }
     }
 
+    async function readNodeAsync(session, nodeId) {
+        try {
+            const dataValue = await session.read({
+                nodeId,
+                attributeId: AttributeIds.Value
+            });
+            if (dataValue.statusCode.name === 'Good') {
+                return dataValue.value.value;
+            } else {
+                console.error(`⚠️  NodeId ${nodeId} exists, but status: ${dataValue.statusCode.name}`);
+            }
+        } catch (error) {
+            console.error(`❌ NodeId ${nodeId} could not be read: ${error.message}`);
+        }
+    }
+
+    async function writeNodeAsync(session, accessString, dataType, value) {
+        try {
+            await session.writeSingleNode(
+                accessString,
+                { dataType, value }
+            );
+        } catch (error) {
+            console.error(`❌ NodeId ${accessString} could not be written: ${error.message}`);
+        }
+    }
+
     class Client {
         constructor(endpointUrl, namespace, nodesConfig) {
             this._endpointUrl = endpointUrl;
-            this._namespace = namespace;
-            this._nodeIds = nodesConfig;
             this._nodes = {};
+            for (const dataId in nodesConfig) {
+                if (nodesConfig.hasOwnProperty(dataId)) {
+                    const rawNodeId = nodesConfig[dataId];
+                    const accessString = getAccessString(namespace, rawNodeId);
+                    const nodeId = resolveNodeId(accessString);
+                    this._nodes[dataId] = { dataId, rawNodeId, accessString, nodeId, value: null, onRefresh: null };
+                }
+            }
             this._client = OPCUAClient.create({ endpointMustExist: true });
             this._session = null;
         }
 
-        get type() {
-            return 'task';
-        }
-
         Initialize(onSuccess, onError) {
             const tasks = [];
-            tasks.push((onSuc, onErr) => startOpcuaClient(this._client, this._endpointUrl, this._namespace, this._nodeIds, this._nodes).then(session => {
+            tasks.push((onSuc, onErr) => startOpcuaClientAsync(this._client, this._endpointUrl, this._nodes).then(session => {
                 this._session = session;
                 onSuc();
             }).catch(onErr));
+            tasks.push((onSuc, onErr) => {
+                this._subscription = ClientSubscription.create(this._session, {
+                    requestedPublishingInterval: 1000,   // ms
+                    requestedLifetimeCount: 100,
+                    requestedMaxKeepAliveCount: 10,
+                    maxNotificationsPerPublish: 100,
+                    publishingEnabled: true,
+                    priority: 10
+                });
+                this._subscription.on('started', () => {
+                    console.log('Subscription started - ID:', this._subscription.subscriptionId);
+                }).on('terminated', () => {
+                    console.log('Subscription terminated');
+                });
+                onSuc();
+            });
             Executor.run(tasks, () => {
-                console.log(`Nodes: ${JSON.stringify(this._nodes, undefined, 2)}`);
+                // console.log(`Nodes: ${JSON.stringify(this._nodes, undefined, 2)}`);
                 onSuccess();
             }, error => {
                 onError(error);
@@ -125,136 +208,129 @@
 
         Shutdown(onSuccess, onError) {
             if (this._session) {
-                stopOpcuaClient(this._session, this._client).then(onSuccess);
+                stopOpcuaClientAsync(this._session, this._client, this._subscription, this._nodes).then(onSuccess);
             } else {
                 onError('Session not started');
             }
         }
 
         GetType(dataId) {
-            // TODO
+            const node = this._nodes[dataId];
+            return node ? node.type : Core.DataType.Unknown;
         }
 
         SubscribeData(dataId, onRefresh) {
-            // TODO
+            const node = this._nodes[dataId];
+            if (!node) {
+                throw new Error(`Unknown data id: '${dataId}'`);
+            } else if (node.onRefresh === onRefresh) {
+                console.error(`Node with data id: '${dataId}' is already subscribed with same onRefresh(value) callback`);
+            } else {
+                node.onRefresh = onRefresh;
+                /*
+                node.monitoredItem = this._subscription.monitor(
+                    {
+                        nodeId: node.nodeId,
+                        attributeId: AttributeIds.Value
+                    },
+                    {
+                        samplingInterval: 500,  // ms
+                        discardOldest: true,
+                        queueSize: 10
+                    },
+                    TimestampsToReturn.Both
+                );
+                node.monitoredItem.on('changed', dataValue => {
+                    const value = dataValue.value.value;
+                    console.log(`Value changed: ${value}`);
+                    try {
+                        node.onRefresh(value);
+                    } catch (error) {
+                        console.error(`Failed calling onResfresh(value) for id '${node.dataId}'`);
+                    }
+                });
+                */
+                this._subscription.monitor(
+                    {
+                        nodeId: node.nodeId,
+                        attributeId: AttributeIds.Value
+                    },
+                    {
+                        samplingInterval: 500,  // ms
+                        discardOldest: true,
+                        queueSize: 10
+                    },
+                    TimestampsToReturn.Both
+                ).then(monitoredItem => {
+                    console.log(`Monitored id '${node.dataId}'`);
+                    node.monitoredItem = monitoredItem;
+                    monitoredItem.on('changed', dataValue => {
+                        const value = dataValue.value.value;
+                        console.log(`Value changed: ${value}`);
+                        try {
+                            node.onRefresh(value);
+                        } catch (error) {
+                            console.error(`Failed calling onResfresh(value) for id '${node.dataId}'`);
+                        }
+                    });
+                }).catch(error => console.error(`Failed to monitor '${node.dataId}': ${error.message}`));
+            }
         }
 
         UnsubscribeData(dataId, onRefresh) {
-            // TODO
+            const node = this._nodes[dataId];
+            if (!node) {
+                throw new Error(`Unknown data id: '${dataId}'`);
+            } else if (node.onRefresh !== onRefresh) {
+                console.error(`Node with data id: '${dataId}' is not subscribed with passed onRefresh(value) callback`);
+            } else {
+                node.onRefresh = null;
+                if (node.monitoredItem) {
+                    node.monitoredItem.terminate().then(() => {
+                        console.log(`Un-monitored id '${node.dataId}'`);
+                    }).catch(error => console.error(`Failed to un-monitor '${node.dataId}': ${error.message}`));
+                }
+            }
         }
 
         Read(dataId, onResponse, onError) {
-            // TODO
+            const node = this._nodes[dataId];
+            if (!node) {
+                throw new Error(`Unknown data id: '${dataId}'`);
+            }
+            readNodeAsync(this._session, node.nodeId).then(value => {
+                console.log(`✅ Value ${value} read from node '${node.rawNodeId}'`);
+                onResponse(value);
+            }).catch(error => {
+                console.error(`❌ Cannot read from node ${node.rawNodeId}: ${error.message}`);
+                onError(`Cannot read from node ${node.rawNodeId}: ${error.message}`);
+            });
         }
 
         Write(dataId, value) {
-            // TODO
+            const node = this._nodes[dataId];
+            if (!node) {
+                throw new Error(`Unknown data id '${dataId}' fro write`);
+            }
+            writeNodeAsync(this._session, node.accessString, node.rawType, value).then(() => {
+                console.log(`✅ Value ${value} written to node '${node.rawNodeId}'`);
+            }).catch(error => {
+                console.error(`❌ Cannot write value ${value} to node ${node.rawNodeId}: ${error.message}`);
+            });
         }
 
         GetDataPoints() {
-            // TODO
+            const dataPoints = [];
+            for (const dataId in this._nodes) {
+                if (this._nodes.hasOwnProperty(dataId)) {
+                    const node = this._nodes[dataId];
+                    dataPoints.push({ id: dataId, type: node.type });
+                }
+            }
+            return dataPoints;
         }
     }
     OPCUA.Client = Client;
-
-    const TODO = { // TODO: Remove when implemented
-        type: "task",
-        targetId: "",
-        dataPoints: {},
-        GetType: function (dataId) {
-            const dp = this._dataPoints[dataId];
-            return dp ? dp.type : Core.DataType.Unknown;
-        },
-        SubscribeData: function (dataId, onRefresh) {
-            const dp = this._dataPoints[dataId];
-            if (dp) {
-                dp.onRefresh = onRefresh;
-            }
-        },
-        UnsubscribeData: function (dataId, onRefresh) {
-            const dp = this._dataPoints[dataId];
-            if (dp) {
-                dp.onRefresh = null;
-            }
-        },
-        Read: function (dataId, onResponse, onError) {
-            const dp = this._dataPoints[dataId];
-            if (dp) {
-                onResponse(dp.value);
-            } else {
-                onError(`Not supported: '${dataId}'`);
-            }
-        },
-        Write: function (dataId, value) {
-            const dp = this._dataPoints[dataId];
-            if (dp) {
-                dp.value = value;
-                if (dp.onResponse) {
-                    dp.onResponse(value);
-                }
-            }
-        },
-        GetDataPoints: function () {
-            const dps = [];
-            for (const id in this.dataPoints) {
-                if (this.dataPoints.hasOwnProperty(id)) {
-                    const type = this.dataPoints[id];
-                    dps.push({
-                        id,
-                        type: Core.DataType[type]
-                    });
-                }
-            }
-            return dps;
-        },
-        _getRandomValue: "include:$Core/Simulation/getRandomValue.j",
-        build: (that, onSuccess, onError) => {
-            that._dataPoints = {};
-            for (const id in that.dataPoints) {
-                if (that.dataPoints.hasOwnProperty(id)) {
-                    const type = Core.DataType[that.dataPoints[id]];
-                    that._dataPoints[id] = {
-                        type,
-                        value: that._getRandomValue(type),
-                        onRefresh: null
-                    }
-                }
-            }
-            onSuccess();
-        },
-        prepare: (that, onSuccess, onError) => {
-            try {
-                that.hmi.env.router.RegisterDataAccesObject(that.targetId, that);
-                onSuccess()
-            } catch (error) {
-                onError(error);
-            }
-        },
-        destroy: (that, onSuccess, onError) => {
-            try {
-                that.hmi.env.router.UnregisterDataAccesObject(that.targetId, that);
-                onSuccess()
-            } catch (error) {
-                onError(error);
-            }
-        },
-        _time: 0,
-        refresh: function (that, date) {
-            const time = date.getTime();
-            if (time > that._time + 500) {
-                that._time = time;
-                for (const id in that._dataPoints) {
-                    if (that._dataPoints.hasOwnProperty(id)) {
-                        const dp = that._dataPoints[id];
-                        dp.value = that._getRandomValue(dp.type);
-                        if (dp.onRefresh) {
-                            dp.onRefresh(dp.value);
-                        }
-                    }
-                }
-            }
-        }
-    };
 
     Object.freeze(OPCUA);
     if (isNodeJS) {
