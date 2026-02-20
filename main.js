e86eaba895608d9fbb4b0969cc90b8cafe024bf8
Markus Hardardt <markus.hardardt@gmx.ch>
2026-02-21 00:00:55 +0100

    refactoring

--- main.js
+++ main.js
@@ -1,21 +1,15 @@
 (function () {
     "use strict";
-    const OPCUA = require('./src/OPCUA.js');
-    const fs = require('fs');
-    const xlsx = require('xlsx');
-
-    // Determine config file
     let configFile = './config.json';
     if (process.argv.length > 2 && /\.json$/.test(process.argv[2])) {
         configFile = /^\.\//.test(process.argv[2]) ? process.argv[2] : './' + process.argv[2];
     }
     const config = require(configFile);
     config.external = Object.freeze({
-        OPCUA,
-        fs,
-        xlsx
+        OPCUA: require('./src/OPCUA.js'),
+        fs: require('fs'),
+        xlsx: require('xlsx')
     });
-
     const main = require('@markus.hardardt/js_utils/server/main.js');
     main(config);
 }());
